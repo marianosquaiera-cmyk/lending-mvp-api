eb76e8eff592c38b7a11a69fa04ebeda269c48fc
marianosquaiera-cmyk <marianosquaiera@gmail.com>
2025-12-01 17:35:33 -0300

    Update merchants.module.ts

--- src/modules/merchants/merchants.module.ts
+++ src/modules/merchants/merchants.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
-import { MerchantsService } from './merchants.service';
 import { MerchantsController } from './merchants.controller';
-import { TiendanubeModule } from '../tiendanube/tiendanube.module';
+import { MerchantsService } from './merchants.service';
+import { PrismaService } from '../../database/prisma.service';
 
 @Module({
-  imports: [TiendanubeModule],
-  providers: [MerchantsService],[TiendanubeService],[AfipService],[BCRAService],[PrismaService],
   controllers: [MerchantsController],
+  providers: [MerchantsService, PrismaService],
   exports: [MerchantsService],
 })
 export class MerchantsModule {}
