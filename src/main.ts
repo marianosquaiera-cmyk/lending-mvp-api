fcd36cf57e2f4132994b203c0617aca39ba39ff8
marianosquaiera-cmyk <marianosquaiera@gmail.com>
2025-12-01 17:14:09 -0300

    Update main.ts
    chore: use PORT env for Render.

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api');
-  const port = process.env.PORT || 3000;
+
+  const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
   await app.listen(port);
-  Logger.log(`🚀 Lending MVP API running on http://localhost:${port}/api`);
 }
 bootstrap();
